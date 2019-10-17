422d25c7c91393f2d8b5027b682eedcc73f0ae37
alti21 <altemush@gmail.com>
2019-10-17 12:29:07 -0700

    Added game over functionality
    Game is over once a player reaches the winning score. Once this happens, scores cannot be updated. Currently winning score value is hardcoded.

--- score_keeper.js
+++ score_keeper.js
@@ -4,15 +4,34 @@ var p1Display = document.querySelector("#p1Display");
 var p2Display = document.getElementById("p2Display");
 var p1Score = 0;
 var p2Score = 0;
+var gameOver = false;
+var winningScore = 5;
 
 p1Button.addEventListener("click", function()
 {
-    p1Score++;
-    p1Display.innerHTML = p1Score;    
+    if(!(gameOver))
+    {
+        p1Score++;
+        if(p1Score === winningScore)
+        {
+            gameOver = true;
+            alert("GAME OVER! Player 1 wins!");
+        }
+        p1Display.innerHTML = p1Score;
+    }
+        
 });
 
 p2Button.addEventListener("click", function()
 {
-    p2Score++;
-    p2Display.innerHTML = p2Score;    
+    if(!(gameOver))
+    {
+        p2Score++;
+        if(p2Score === winningScore)
+        {
+            gameOver = true;
+            alert("GAME OVER! Player 2 wins!");
+        }
+        p2Display.innerHTML = p2Score;
+    }    
 });
