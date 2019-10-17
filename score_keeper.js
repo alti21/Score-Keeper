533ac7b0364e492eb662cf15fce3f3b257e7ad75
alti21 <altemush@gmail.com>
2019-10-17 12:21:28 -0700

    Added functionality to update player two's score
    Added listener to change player two's score

--- score_keeper.js
+++ score_keeper.js
@@ -1,9 +1,18 @@
 var p1Button = document.querySelector("#p1");
-var p2Buttton = document.getElementById("p2");
+var p2Button = document.getElementById("p2");
+var p1Display = document.querySelector("#p1Display");
+var p2Display = document.getElementById("p2Display");
 var p1Score = 0;
+var p2Score = 0;
 
 p1Button.addEventListener("click", function()
 {
     p1Score++;
-    document.querySelector("#p1Display").innerHTML = p1Score;    
+    p1Display.innerHTML = p1Score;    
+});
+
+p2Button.addEventListener("click", function()
+{
+    p2Score++;
+    p2Display.innerHTML = p2Score;    
 });
